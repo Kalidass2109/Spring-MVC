7c8f086d9cf8367c3fe77a93c16fd42eea41d46b
Kalidass2109 <65493297+Kalidass2109@users.noreply.github.com>
2024-05-07 23:06:54 +0530

    Add files via upload
    Spring MVC

---
+++ 31_WebMVC_JSP_App/src/main/java/com/kali/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.kali.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/")
+	public String welcome(Model model) {
+		
+		model.addAttribute("msg", "Good Morning...!!");
+		return "index";
+	}
+
+}
---
+++ 32_SB_Actuator_App/src/main/java/com/kali/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.kali.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/welcome")
+	@ResponseBody
+	public String welcomeMsg() {
+		return "Welcome to Spring Boot";
+	}
+
+}
